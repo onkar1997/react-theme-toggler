c4777d85a859998cd4072528176537dff5ef15dc
DESKTOP-51379U5\DELL <onkarpawar25@gmail.com>
2023-06-06 19:24:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { ThemeContext } from './themeContext'
 
 function App() {
+  const { theme, toggle, dark } = React.useContext(ThemeContext)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header
+        className="App-header"
+        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <button
+          type="button"
+          onClick={toggle}
+          style={{
+            color: theme.color,
+            outline: 'none'
+          }}
         >
+          Toggle to {!dark ? 'Dark' : 'Light'} theme
+        </button>
+        
+        <p>
           Learn React
-        </a>
+        </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
